3eff61c751bdde523a1d4a976e9dbf3352dc38b0
ashisherikilla <134827407+ashisherikilla@users.noreply.github.com>
2026-08-29 14:13:57 +0530

    fix:frontend url in main.py to resolve CORS policy

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.api.chat import router as chat_router
 from app.api.documents import router as document_router
 
-
 # ============================================================
 # FASTAPI APPLICATION
 # ============================================================
@@ -37,7 +36,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://docintel-frontend-mocha.vercel.app",
+        "https://docintel-rag-based.vercel.app",
+        "https://docintel-irvc851bv-erikilla-ashishs-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -49,18 +49,15 @@ app.add_middleware(
 # API ROUTERS
 # ============================================================
 
-app.include_router(
-    chat_router
-)
+app.include_router(chat_router)
 
-app.include_router(
-    document_router
-)
+app.include_router(document_router)
 
 
 # ============================================================
 # HEALTH CHECK
 # ============================================================
+
 
 @app.get("/health")
 def health_check():
